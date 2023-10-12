b39503383a87999fb1f7f3465300b578b2cfe99e
ArsenySamoylov <samoylov@ispras.ru>
2023-10-13 02:04:13 +0300

    env_alloc: added allocation of stack for process

--- kern/env.c
+++ kern/env.c
@@ -111,6 +111,8 @@ env_init(void) {
  *    -E_NO_FREE_ENV if all NENVS environments are allocated
  *    -E_NO_MEM on memory exhaustion
  */
+void
+map_addr_early_boot(uintptr_t va, uintptr_t pa, size_t sz);
 int
 env_alloc(struct Env **newenv_store, envid_t parent_id, enum EnvType type) {
 
@@ -156,7 +158,11 @@ env_alloc(struct Env **newenv_store, envid_t parent_id, enum EnvType type) {
     env->env_tf.tf_cs = GD_KT;
 
     // LAB 3: Your code here:
-    // static uintptr_t stack_top = 0x2000000;
+    static uintptr_t stack_top = 0x2000000;
+    env->env_tf.tf_rsp = stack_top;
+    map_addr_early_boot(stack_top, stack_top, PAGE_SIZE*2);
+    stack_top += PAGE_SIZE*2;
+
 #else
     env->env_tf.tf_ds = GD_UD | 3;
     env->env_tf.tf_es = GD_UD | 3;
@@ -173,6 +179,7 @@ env_alloc(struct Env **newenv_store, envid_t parent_id, enum EnvType type) {
     *newenv_store = env;
 
     if (trace_envs) cprintf("[%08x] new env %08x\n", curenv ? curenv->env_id : 0, env->env_id);
+     cprintf("%s: tf_rsp: %p\n", __func__, (void*) env->env_tf.tf_rsp);   
     return 0;
 }
 
@@ -230,8 +237,7 @@ bind_functions(struct Env *env, uint8_t *binary, size_t size, uintptr_t image_st
  *   You must also do something with the program's entry point,
  *   to make sure that the environment starts executing there.
  *   What?  (See env_run() and env_pop_tf() below.) */
-void
-map_addr_early_boot(uintptr_t va, uintptr_t pa, size_t sz);
+
 
 static int
 load_icode(struct Env *env, uint8_t *binary, size_t size) {
@@ -284,6 +290,8 @@ load_icode(struct Env *env, uint8_t *binary, size_t size) {
 void
 env_create(uint8_t *binary, size_t size, enum EnvType type) {
     // LAB 3: Your code here
+    // cprintf("%s %p\n", __func__, binary);
+
     struct Env* nenv = NULL;
     int status = 0;
 
@@ -294,7 +302,8 @@ env_create(uint8_t *binary, size_t size, enum EnvType type) {
 
     status = load_icode (nenv, binary, size);
     if (status) {
-        cprintf ("%s: %i\n", __func__, status);   
+        cprintf ("%s: %i\n", __func__, status);
+        assert(0);   
     }
 
     nenv->env_parent_id = 0;
@@ -419,6 +428,7 @@ env_run(struct Env *env) {
     curenv->env_status = ENV_RUNNING;
     curenv->env_runs++;
 
+    // env->env_tf.tf_rsp = read_rsp();
     env_pop_tf(&env->env_tf);
 
     panic("Reached unrecheble\n");    
