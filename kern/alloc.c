736b8a1f5c1f543d2c29c02ec2752b3dbc7dd22b
ArsenySamoylov <samoylov@ispras.ru>
2023-11-09 17:44:01 +0300

    Alloc: added spinlock

--- kern/alloc.c
+++ kern/alloc.c
@@ -23,12 +23,20 @@ check_list(void) {
     asm volatile("sti");
 }
 
+static struct spinlock allocator_lock = { 
+#if trace_spinlock
+    .name = "allocator_lock"
+#endif
+};
+
 /* malloc: general-purpose storage allocator */
 void *
 test_alloc(uint8_t nbytes) {
 
     /* Make allocator thread-safe with the help of spin_lock/spin_unlock. */
     // LAB 5: Your code here:
+   
+    spin_lock(&allocator_lock);
 
     size_t nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
 
@@ -58,14 +66,19 @@ test_alloc(uint8_t nbytes) {
                 p += p->size;
                 p->size = nunits;
             }
+
+            spin_unlock(&allocator_lock);
             return (void *)(p + 1);
         }
 
         /* wrapped around free list */
         if (p == freep) {
+            spin_unlock(&allocator_lock);
             return NULL;
         }
     }
+
+    spin_unlock(&allocator_lock);
 }
 
 /* free: put block ap in free list */
@@ -77,6 +90,7 @@ test_free(void *ap) {
 
     /* Make allocator thread-safe with the help of spin_lock/spin_unlock. */
     // LAB 5: Your code here
+    spin_lock(&allocator_lock);
 
     /* freed block at start or end of arena */
     Header *p = freep;
@@ -104,4 +118,5 @@ test_free(void *ap) {
     freep = p;
 
     check_list();
+    spin_unlock(&allocator_lock);
 }
